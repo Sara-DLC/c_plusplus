7327331da1249d93308245fbbabdb9190903c2f0
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-09-07 01:14:14 -0500

    While loop

---
+++ While/src/While.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int count = 0;
+
+	while (count < 5) {
+		cout << "Hello " << count << endl;
+
+		count ++; //increments count by 1
+	}
+
+	cout << "Program Quitting..." << endl;
+
+	return 0;
+}
