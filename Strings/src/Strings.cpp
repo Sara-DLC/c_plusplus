8d6e101f53a04868fbf2cf288e69d50b44707e6c
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-09-07 01:13:37 -0500

    Strings

---
+++ Strings/src/Strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string text1 = "Hello ";
+	string text2 = "Friend";
+
+	cout << text1 << text2 << endl;
+
+	string text3 = text1 + text2;
+
+	cout << text3 << endl;
+
+
+	return 0;
+}
