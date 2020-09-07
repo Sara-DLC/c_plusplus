9be29895358a9a506859402e18aefec661b4d78a
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-09-07 01:02:16 -0500

    Char Array Files

---
+++ CharArrays/src/CharArrays.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//create a char string and output it
+//output all the chars with a for loop
+//output all the chars with a while true loop
+
+int main() {
+
+	char text[] = "hello";
+
+	for (int i = 0; i < sizeof(text); i ++) {
+		cout << text[i] << endl;;
+	}
+
+	int k = 0;
+
+	while(true) {
+		if (text[k] == 0)
+			break;
+
+		cout << text[k] << flush;
+
+		k++;
+	}
+
+	return 0;
+}
