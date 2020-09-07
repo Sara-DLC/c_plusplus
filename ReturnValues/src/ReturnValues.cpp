27b92725f9e1ad96589bb6e01db857140eeebb4f
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-09-07 01:12:50 -0500

    Return Values

---
+++ ReturnValues/src/ReturnValues.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+	cout << "1. Search" << endl;
+	cout << "2. View Record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+//int will return an integer from input
+int inputSelection() {
+
+	cout << "Enter Selection: " << flush;
+
+	int input;
+	cin >> input;
+
+	return input;
+
+}
+
+//Make your main pretty straightforward
+int main() {
+
+	showMenu();
+	//declare variable and assign the processSelection function and pass it to the switch
+	//statement
+	int selection = inputSelection();
+
+	switch (selection) {
+	case 1:
+		cout << "Searching..." << endl;
+		break;
+	case 2:
+		cout << "Viewing..." << endl;
+		break;
+	case 3:
+		cout << "Quitting..." << endl;
+		break;
+	default:
+		cout << "Option not on menu" << endl;
+
+	}
+
+	return 0;
+}
